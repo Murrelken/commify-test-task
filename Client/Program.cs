7d10118ffe849b31b1b3d5114007fa45c640bcc2
Murrelken <danilbutin@gmail.com>
2023-11-11 20:09:11 +0100

    init with front-end

---
+++ Client/Program.cs
@@ -0,0 +1,20 @@
+using commify_test_task.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace commify_test_task.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
---
+++ Shared/Dtos/TaxCalculationResults.cs
@@ -0,0 +1,12 @@
+﻿namespace commify_test_task.Shared.Dtos
+{
+    public class TaxCalculationResults
+    {
+        public decimal GrossAnnualSalary { get; set; }
+        public decimal GrossMonthlySalary { get; set; }
+        public decimal NetAnnualSalary { get; set; }
+        public decimal NetMonthlySalary { get; set; }
+        public decimal AnnualTaxPaid { get; set; }
+        public decimal MonthlyTaxPaid { get; set; }
+    }
+}
