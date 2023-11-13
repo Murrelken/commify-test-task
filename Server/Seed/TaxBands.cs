515ff1e18b06221a9153db609fa38ef4157af69c
Murrelken <danilbutin@gmail.com>
2023-11-13 13:18:45 +0100

    move seed to separate file

---
+++ Server/Seed/TaxBands.cs
@@ -0,0 +1,14 @@
+﻿using commify_test_task.Server.Features.UKIncomeTaxCalculator.Entities;
+
+namespace commify_test_task.Server.Seed
+{
+	public static class TaxBands
+	{
+		public static List<UKTaxBand> DefaultTaxBands => new List<UKTaxBand>
+			{
+				new UKTaxBand(0, 5000, 0),
+				new UKTaxBand(5000, 20000, 20),
+				new UKTaxBand(20000, null, 40)
+			};
+	}
+}
